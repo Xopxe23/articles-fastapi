2bf71a757c7056671c803e47e6494c285d28fea1
George Dudaev <georgeduday@gmail.com>
2023-09-11 01:29:42 +0300

    create migrations alembic for User

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from app.config import settings
 
 
 DATABASE_URL = (f"postgresql+asyncpg:"
-                f"{settings.DB_USER}:{settings.DB_PASS}/{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}")
+                f"//{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}")
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
